2bcb79c1881619b92d078712d68a547bfd575bf6
y0urmercy <lena.sherstuk2006@mail.ru>
2023-10-10 04:55:51 +0300

    br

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,78 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <div :class="{'active': isActive, 'disabled': !isActive}">test</div>
+  <button @click="isActive = !isActive">click</button>
+  <div :style="customStyles">hhhhh</div>
+  <button :class="buttonClass" @click="change">{{buttonText}}</button>
+</div>
+<div :class="'progress'">
+  <div :style="progressStyles"></div>
+</div>
+<button @click="changepr(10)">10</button>
+<button @click="changepr(40)">40</button>
+<button @click="changepr(90)">90</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      isActive: false,
+      buttonText: "click",
+      progress: 50,
+    }
+  },
+  computed: {
+    customStyles() {
+      return {
+        backgroundColor: 'green',
+        color: 'white',
+        fontSize: '16px'
+      }
+    },
+    buttonClass(){
+      return {
+        'active': this.isActive,
+        'disabled': !this.isActive
+      }
+    },
+    progressStyles(){
+      return {
+        width: this.progress + '%',
+        backgroundColor: 'blue',
+      }
+    },
+
+  },
+  methods: {
+    change(){
+      this.isActive = !this.isActive;
+    },
+    changepr(numProgress){
+      this.progress = numProgress;
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.active {
+  background-color: red;
+}
+
+.disabled {
+  opacity: 0.5;
+  cursor: not-allowed;
+}
+
+.progress{
+  width: 100%;
+  height: 20px;
+  background-color: lightgreen;
+}
+.progress > div {
+  height: 100%;
 }
 </style>
